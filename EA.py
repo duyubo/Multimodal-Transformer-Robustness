276c69f60f0adcc4da1cb827893c827dd7922c85
duyubo <2669561343@qq.com>
2022-05-11 21:50:52 -0400

    fix bug in EA

--- EA.py
+++ EA.py
@@ -35,10 +35,10 @@ class EvolutionSearch:
         self.test_loader =  test_loader
         self.criterion = hyper_params.criterion
         self.modality_list = hyper_params.modality_list
+        
         """constraints """
         self.model = parent_model
         self.latency_constraint = 100
-
 
     """mutate"""
     def mutate(self, sample):
@@ -46,13 +46,13 @@ class EvolutionSearch:
         sample = [active_cross, active_cross_output]
         """
         while True:
-            new_sample = copy.copy(sample)
+            new_sample = copy.deepcopy(sample)
             probs = torch.rand(len(sample[1]),)
             for i in range(len(probs)):
                 if probs[i] < self.mutate_prob:
                     subset_temp = self.model.gen_active_cross(active_modality = self.active_modality)
-                    new_sample[0][i] = copy.copy(subset_temp[0][i])
-                    new_sample[1][i] = copy.copy(subset_temp[1][i])
+                    new_sample[0][i] = copy.deepcopy(subset_temp[0][i])
+                    new_sample[1][i] = copy.deepcopy(subset_temp[1][i])
             """to be modified"""
             efficiency = 0
             if efficiency <= self.latency_constraint:
@@ -61,18 +61,23 @@ class EvolutionSearch:
     """crossover"""
     def crossover(self, sample1, sample2):
         while True:
-            new_sample = copy.copy(sample1)
+            new_sample = copy.deepcopy(sample1)
             for i in range(len(new_sample[0])):
                 random_i = random.choice([0, 1])
                 if random_i == 0:
-                  new_sample[0][i] = copy.copy(sample2[0][i])
-                  new_sample[1][i] = copy.copy(sample2[1][i])
-
+                  new_sample[0][i] = copy.deepcopy(sample2[0][i])
+                  new_sample[1][i] = copy.deepcopy(sample2[1][i])
             """to be modified"""
             efficiency = 0
             if efficiency <= self.latency_constraint:
                 return new_sample, efficiency
-    
+    def get_acc(self, sample):
+        self.model.set_active_modalities(
+                  active_modality = self.active_modality,
+                  active_cross = copy.deepcopy(sample[0]), 
+                  active_cross_output = copy.deepcopy(sample[1]))    
+        acc = self.eval_model()
+        return acc
     """search"""
     def search(self):
         mutation_numbers = int(round(self.mutation_ratio * self.population_size))
@@ -80,40 +85,28 @@ class EvolutionSearch:
 
         best_valids = [-10]
         population = []  # (validation, sample, latency) tuples
-        child_pool = []
-        efficiency_pool = []
         best_info = None
         
         print("Generate random population...")
-        child_pool.append([[['ta', 'tv'], ['at', 'av'], ['vt', 'va']],[['ta', 'tv'], ['at', 'av'], ['vt', 'va']]])
-        for _ in range(self.population_size - 1):
+        for _ in range(self.population_size):
             active_cross, active_cross_output = self.model.gen_active_cross(active_modality = self.active_modality)
             sample = [active_cross, active_cross_output]
-            child_pool.append(sample)
-       
-        print("Get population accuracy")
-        for i in range(self.population_size):
-            self.model.set_active_modalities(
-                  active_modality = self.active_modality,
-                  active_cross = child_pool[i][0], 
-                  active_cross_output = child_pool[i][1])    
-            err = self.eval_model()
-            print(err, child_pool[i][1])
-            population.append((err, child_pool[i]))
+            acc = self.get_acc(sample) 
+            population.append([acc, sample])
+            print(acc, sample[1])
 
         print("Start Evolution...")
         # After the population is seeded, proceed with evolving the population.
         for i in range(self.max_time_budget):
-                parents = sorted(population, key=lambda x: x[0])[::-1][:parents_size]
-                err = parents[0][0]
-                print("Iter: {} Err: {}".format(i, err))
-                print(parents)
-                if err > best_valids[-1]:
-                    best_valids.append(err)
-                    best_info = parents[0]
+                parents = sorted(population, key=lambda x: x[0])[::-1][:parents_size]  
+                acc = parents[0][0]
+                print("Iter: {} Acc: {}".format(i, acc), parents[0])
+                if acc > best_valids[-1]:
+                    best_valids.append(acc)
+                    best_info = copy.deepcopy(parents[0])
                 else:
                     best_valids.append(best_valids[-1])
-                print('best info in ', i, ':', best_info)    
+                #print('best info in ', i, ':', best_info)    
                 if i >= self.max_time_budget - 1:
                     self.model.set_active_modalities(
                       active_modality = self.active_modality,
@@ -122,35 +115,35 @@ class EvolutionSearch:
                     self.eval_model(test = True)
                     return best_valids, best_info
 
-                population = parents
-                child_pool = []
+                population = copy.deepcopy(parents)
 
-                '''Mutate'''
+                """ Mutate """
                 for j in range(mutation_numbers):
                     par_sample = population[np.random.randint(parents_size)][1]
                     new_sample, efficiency = self.mutate(par_sample)
-                    child_pool.append(new_sample)
-                
-                """Crossover"""
+                    acc = self.get_acc(new_sample) 
+                    population.append(
+                        [acc, new_sample]
+                    ) 
+                    
+                """ Crossover """
                 for j in range(self.population_size - mutation_numbers):
                     par_sample1 = population[np.random.randint(parents_size)][1]
                     par_sample2 = population[np.random.randint(parents_size)][1]
-                    new_sample, efficiency = self.crossover(
-                        par_sample1, par_sample2
-                    )
-                    child_pool.append(new_sample)
-
-                """Get accuracys for newly added population"""
-                for j in range(self.population_size):
-                    self.model.set_active_modalities(
-                      active_modality = self.active_modality,
-                      active_cross = child_pool[j][0], 
-                      active_cross_output = child_pool[j][1])   
-                    err = self.eval_model()
-                    population.append(
-                        (err, child_pool[j])
-                    )
-
+                    new_sample, efficiency = self.crossover(par_sample1, par_sample2)
+                    acc = self.get_acc(new_sample) 
+                    population.append([acc, new_sample])
+    
+    """test with given modality combinations"""
+    def test_modality(self, active_code):
+        self.model.set_active_modalities(
+                    active_modality = self.active_modality,
+                    active_cross = active_code[0], 
+                    active_cross_output = active_code[1])    
+        acc = self.eval_model()
+        print(acc)
+        acc = self.eval_model(test = True)
+        return acc
         
     def eval_model(self, test = False):
         hyp_params = self.hyper_params 
@@ -168,18 +161,11 @@ class EvolutionSearch:
                 preds = self.model([text, audio, vision]) #
                 results.append(preds.cpu().detach())
                 truths.append(eval_attr.cpu().detach())
-                
         results = torch.cat(results)
         truths = torch.cat(truths)
-        test_preds = results.view(-1).numpy()
-        test_truth = truths.view(-1).numpy()
-
-        non_zeros = np.array([i for i, e in enumerate(test_truth) if e != 0])
-        binary_truth = (test_truth[non_zeros] > 0)
-        binary_preds = (test_preds[non_zeros] > 0)
         if test:
-          eval_mosei_senti(results, truths, True)
-        return accuracy_score(binary_truth, binary_preds)
+          eval_mosei_senti(results, truths,  exclude_zero = True)
+        return binary_acc(results, truths, exclude_zero = True) + mosei_multiclass_acc(results, truths)
     
 import sys
 import torch
@@ -206,7 +192,7 @@ parser.add_argument('--seed', type=int, default=1111,
 parser.add_argument('--no_cuda', action='store_true',
                     help='do not use cuda')
 
-parser.add_argument('--mutate_prob', type=float, default=0.8,
+parser.add_argument('--mutate_prob', type=float, default=0.5,
                     help='mutate_prob')
 parser.add_argument('--parent_ratio', type=float, default=0.8,
                     help='parent_ratio')
@@ -216,7 +202,7 @@ parser.add_argument('--subnet_prob', type=float, default=0.5,
                     help='subnet_prob')
 parser.add_argument('--population_size', type=int, default=100,
                     help='population_size')
-parser.add_argument('--max_time_budget', type=float, default=200,
+parser.add_argument('--max_time_budget', type=int, default=200,
                     help='max_time_budget')  
 parser.add_argument('--active_modality', type=list, default=[0, 1, 2],
                     help='active_modality')        
@@ -251,8 +237,8 @@ if torch.cuda.is_available():
 ####################################################################
 
 print("Start loading the data....")
-valid_data = get_data(args, dataset, 'valid')
-test_data = get_data(args, dataset, 'test')
+valid_data = get_data(args, 'valid')
+test_data = get_data(args, 'test')
 
 valid_loader = DataLoader(valid_data, batch_size = args.batch_size, shuffle = False)
 test_loader = DataLoader(test_data, batch_size = args.batch_size, shuffle = False)
@@ -267,7 +253,7 @@ print('Finish loading the data....')
 hyp_params = args
 hyp_params.use_cuda = use_cuda
 hyp_params.orig_d_l, hyp_params.orig_d_a, hyp_params.orig_d_v = valid_data.get_dim()
-hyp_params.l_len, hyp_params.a_len, hyp_params.v_len = valid_data.get_seq_len()
+hyp_params.l = valid_data.get_seq_len()
 hyp_params.output_dim = output_dim_dict.get(dataset, 1)
 hyp_params.criterion = torch.nn.L1Loss()
 hyp_params.n_valid, hyp_params.n_test = len(valid_data), len(test_data)
@@ -276,5 +262,5 @@ print(hyp_params.n_valid, hyp_params.n_test)
 if __name__ == '__main__':
     parent_model = torch.load(hyp_params.model_path)
     e = EvolutionSearch(parent_model, hyper_params = hyp_params, valid_loader = valid_loader, test_loader = test_loader)
-    e.search()
-
+    best_selection = e.search()
+    #e.test_modality([[[], ['at', 'atv'], ['vt', 'va', 'vta']], [['t'], ['at'], ['v', 'vt', 'va']]])
